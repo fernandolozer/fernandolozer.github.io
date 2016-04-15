c8665b36ff246e4a5f4f7023cf66833526858ce3
Fernando Lozer Lichtenheld <flichtenheld@gmail.com>
2016-04-15 00:08:47 -0300

    Alteracoes exercicio

--- aulas/20160407/code.js
+++ aulas/20160407/code.js
@@ -2,95 +2,144 @@ var game;
 var avatar;
 var myObstacles = [];
 
-function startGame(){
-	game = new Game(document.getElementById("game"),480,270,updateFunc,20);
-	avatar = new GameObject(30,30,"red", 10, 120);
-	score = new GameObject("30px","Consolas","black",280,40,"text");
-	game.start();
+function startGame() {
+    game = new Game(document.getElementById("game"), 480, 270, updateFunc, 20);
+    avatar = new GameObject(30, 30, "red", 10, 120);
+    avatar.gravity = 0.075;
+    score = new GameObject("30px", "Consolas", "black", 280, 40, "text");
+    game.start();
 }
 
-function updateFunc(){
+function updateFunc() {
+    var x, height, gap, minHeight, minGap, maxGap, maxHeight;
+    for (var i = 0; i < myObstacles.length; i++) {
+        if (avatar.crashWith(myObstacles[i])) {
+            return;
+        }
+    }
+    game.clear();
+    game.frameno++;
+    if (game.frameno == 1 || everyInterval(150)) {
+        x = game.canvas.width;
+        minHeight = 20;
+        maxHeight = 200;
+        height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+        // insere no vetor de obstaculos
+        // parte de cima
+        myObstacles.push(new GameObject(
+            10,
+            height,
+            "green",
+            x,
+            0
+        ));
+        // parte de baixo
+        myObstacles.push(new GameObject(
+            10,
+            game.canvas.height - height - gap,
+            "green",
+            x,
+            height + gap
+        ));
+    }
+    score.text = "SCORE: " + game.frameno;
+    score.update(game.context);
+    avatar.newPos();
+    avatar.update(game.context);
 
+    for (var i = 0; i < myObstacles.length; i++) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update(game.context);
+    }
 }
 
-function everyInterval(n){
-
+// TODO: Almentar a velocidade a cada score de 1000, que pode
+// ser utilizado como o ultimo exercicio da lista.
+// TODO: Criar uma nova funcao que quando tiver keyup, reiniciar o jogo
+function everyInterval(n) {
+    return ((game.frameno / n) % 1 == 0);
 }
 
-window.onkeyup = function(e){
-
+window.onkeyup = function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+    if (key == 32) {
+        avatar.speedY -= 1.9;
+    }
 }
 
-function Game(canvas, width, height, updateFunc, time){
-	this.canvas = canvas;
-	this.start = function (){
-		this.canvas.height = height;
-		this.canvas.width = width;
-		this.context = this.canvas.getContext("2d");
-		this.frameno = 0;
-		this.interval = setInterval(updateFunc,time);
-	}
-	this.clear = function(){
-		this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
-	}
+function Game(canvas, width, height, updateFunc, time) {
+    this.canvas = canvas;
+    this.start = function() {
+        this.canvas.height = height;
+        this.canvas.width = width;
+        this.context = this.canvas.getContext("2d");
+        this.frameno = 0;
+        this.interval = setInterval(updateFunc, time);
+    }
+    this.clear = function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
 
-function GameObject(width, height, color, x, y, type){
-	this.type = type;
-	this.width = width;
-	this.height = height;
-	this.speedX = 0;
-	this.speedY = 0;
-	this.x = x;
-	this.y = y;
-	this.gravity = 0;
-	this.gravitySpeed = 0;
-	this.update = function(ctx){
-		if(this.type == "text"){
-			ctx.font = this.width + " " + this.height;
-			ctx.fillStyle = color;
-			ctx.fillText(this.text, this.x, this.y);
-		}
-	}
-	this.newPos = function(){
-		this.gravitySpeed += this.gravity;
-		this.x += this.speedX;
-		this.y += this.speedY + this.gravitySpeed;
-		this.hitBottom();
-		this.hitTop();
-	}
-	this.hitBottom = function(){
-		var bottom = game.canvas.height - this.height;
-		if(this.y > bottom){
-			this.y = bottom;
-		}
-	}
-	this.hitTop = function(){
-		var top = 0;
-		if(this.y < top){
-			this.y = top;
-		}
-	}
-	this.crashWith = function(obj2){
-		var myleft = this.x;
-		var myright = this.x + this.width;
-		var mytop = this.y;
-		var mybottom = this.y + this.height;
+function GameObject(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.gravity = 0;
+    this.gravitySpeed = 0;
+    this.update = function(ctx) {
+        if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+        this.hitTop();
+    }
+    this.hitBottom = function() {
+        var bottom = game.canvas.height - this.height;
+        if (this.y > bottom) {
+            this.y = bottom;
+        }
+    }
+    this.hitTop = function() {
+        var top = 0;
+        if (this.y < top) {
+            this.y = top;
+        }
+    }
+    this.crashWith = function(obj2) {
+        var myleft = this.x;
+        var myright = this.x + this.width;
+        var mytop = this.y;
+        var mybottom = this.y + this.height;
 
-		var otherleft = obj2.x;
-		var otherright = obj2.x + obj2.width;
-		var othertop = obj2.y;
-		var otherbottom = obj2.y + obj2.height;
+        var otherleft = obj2.x;
+        var otherright = obj2.x + obj2.width;
+        var othertop = obj2.y;
+        var otherbottom = obj2.y + obj2.height;
 
-		var crash = true;
-		if( (mybottom < othertop) || (mytop > otherbottom)
-			|| (myright < otherleft) || (myleft > otherright) ){
-			crash = false;
-		}
-		return crash;
-	}
-
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
 }
 
 window.onload = startGame;
-}
--- exercicios/dialTelefone/exemplo.js
+++ exercicios/dialTelefone/exemplo.js
@@ -1,62 +1,55 @@
-var score = [0,0];
-var player= "";
-var ia = "";
+function init() {
+    var num1 = document.getElementById("num1");
+    var num2 = document.getElementById("num2");
+    var num3 = document.getElementById("num3");
+    var num4 = document.getElementById("num4");
+    var num5 = document.getElementById("num5");
+    var num6 = document.getElementById("num6");
+    var num7 = document.getElementById("num7");
+    var num8 = document.getElementById("num8");
+    var num9 = document.getElementById("num9");
+    var num0 = document.getElementById("num0");
 
-var options = ["rock","paper","scizor"];
-
-function init(){
-	var num1 = document.getElementById("num1");
-	var num2 = document.getElementById("num2");
-	var num3 = document.getElementById("num3");
-	var num4 = document.getElementById("num4");
-	var num5 = document.getElementById("num5");
-	var num6 = document.getElementById("num6");
-	var num7 = document.getElementById("num7");
-	var num8 = document.getElementById("num8");
-	var num9 = document.getElementById("num9");
-	var num0 = document.getElementById("num0");
-	
-	p1Rock.onclick = action;
-	p1Paper.onclick = action;
-	p1Scizor.onclick = action;
+    num1.onclick = action;
+    num2.onclick = action;
+    num3.onclick = action;
+    num4.onclick = action;
+    num5.onclick = action;
+    num6.onclick = action;
+    num7.onclick = action;
+    num8.onclick = action;
+    num9.onclick = action;
+    num0.onclick = action;
 }
 
-function iaPlay(){
-	ia = options[Math.floor( Math.random()*3 )];
-	var element = document.getElementById("p2_"+ia).className += " selected";
+
+function action(event) {
+    console.log('event: ' + event);
+    for (var i = 0; i < 10; i++) {
+        var nomeCampo = 'num' + i;
+        if (event.target.id == nomeCampo) {
+            inserirTextoResultado(i);
+        }
+    }
 }
 
-function action(event){
-	zera();
-	var choice = event.target;
-	player = choice.id.split("_")[1];
-	choice.className += " selected";
-	iaPlay();
-	check();
+function inserirTextoResultado(texto) {
+    var resultado = document.getElementById('resultado');
+    resultado.innerHTML = resultado.innerHTML + texto;
 }
 
-function zera(){
-	var options = document.getElementsByClassName("option");
-	for(var i=0; i< options.length; i++){
-		console.log(options)
-		options[i].className = "option";
-	}
+window.onkeyup = function(e) {
+    var key = e.keyCode ? e.keyCode : e.which;
+    if (key >= 48 || key <= 57) {
+        var nomeCampo = 'num' + (key - 48);
+        var evento = e;
+        evento.target.id = nomeCampo;
+        console.log(nomeCampo);
+
+        console.log(evento);
+        action(evento);
+    }
 }
 
-function check(){
-	var result = document.getElementById("result").childNodes[0];
-	if(ia==player){
-		result.src="cat.jpg";
-	}else if( (player == "rock" && ia == "paper") 
-			|| (player == "paper" && ia == "scizor")
-			|| (player == "scizor" && ia == "rock") ) {
-		result.src="lose.jpg";
-		score[1]++;
-	}else{
-		result.src="victory.jpg";
-		score[0]++;
-	}
-	var scorep1;
-}
 
 window.onload = init;
