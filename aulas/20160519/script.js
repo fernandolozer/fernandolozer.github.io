7d4a9bc28f2097369b91f57d645a13a14d2ec549
Fernando Lozer Lichtenheld <flichtenheld@gmail.com>
2016-05-20 01:33:35 +0000

    Fim da Aula

--- aulas/20160519/script.js
+++ aulas/20160519/script.js
@@ -1,6 +1,50 @@
 function init(argument) {
-    console.log("teste");
+    var txtarea = document.getElementById("txt");
+    if (!txtarea.value) {
+        txtarea.value = localStorage.getItem('valorTexto');
+    }
+    
+    
+    // adiciona listener no botao de clear
+    document.getElementById("btnClear").addEventListener("click",
+        function () {
+            localStorage.clear();
+            txtarea.value = '';
+        }
+    );
+  
+    // adiciona listener ao digitar
+    
+    txtarea.addEventListener('keyup', 
+        function(e) {
+            localStorage.setItem('valorTexto', this.value);
+        }
+    );
 }
 
 
+
+
 window.onload = init;
+
+// funcoes abaixo apenas para testes iniciais. não estão sendo usadas na aplicação,
+// mas estão salvas para referencia
+function inicio(argument) {
+    console.log("Inicio da aplicação");
+    
+    
+    var usuario = {user: 'Fernando', id: 15};
+    
+    localStorage.setItem("usr", JSON.stringify(usuario));
+    
+    var data = localStorage.getItem("usr");
+    if (data !== undefined) {
+        data = JSON.parse(data);
+    }
+    console.log( "Ola, " + data.user);
+    // localStorage.clear();
+    // localStorage.removeItem("dataNascimento");
+    
+}
+
+
